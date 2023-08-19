f194ad8031096716033684e414c03d269027f743
Danil Galkin <pixell_tm@bk.ru>
2023-08-19 09:26:36 +0300

    add mauto-migrate in db

--- db/models.py
+++ db/models.py
@@ -1,8 +1,7 @@
-
 from sqlalchemy import Column, Integer, String, ForeignKey, Date, DECIMAL
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base, DeclarativeBase
-from sqlalchemy import create_engine
+
 
 Base: DeclarativeBase = declarative_base()
 
@@ -24,13 +23,10 @@ class Lessons_base(Base):
     user = relationship("User", foreign_keys=id)
     lesson = relationship("Lesson", foreign_keys=id)
 
+
 class Lesson(Base):
     __tablename__ = "lesson"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(String)
     price = Column(DECIMAL)
-
-engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5433/postgres')
-
-Base.metadata.create_all(engine)
